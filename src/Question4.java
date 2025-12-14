83b152dea2ecb3d2d1cb69b627012d79ef9736cb
toscanoGithub <pasarela70@hotmail.com>
2025-12-13 22:26:19 -0500

    Réponse 4

---
+++ src/Question4.java
@@ -0,0 +1,22 @@
+/* Demandez à l’usager d’entrer un nombre entier. S’il est inférieur ou égal à 1, affichez un message d’erreur. Sinon, déterminez et affichez tous les diviseurs de ce nombre.*/
+
+void main() {
+    // Variables et collecte des données
+    String saisie = IO.readln("Entrez un nombre positif: ");
+    int saisieConvertie = Integer.parseInt(saisie);
+
+    // Traitement
+    if(saisieConvertie <= 1) {
+        IO.println("Erreur!");
+    } else {
+        int compteur = 1;
+        while (compteur <= saisieConvertie / 2) { // Pas besoin d'aller plus que la moitié de saisieConvertie
+            if (saisieConvertie % compteur == 0) {
+                IO.println(compteur);
+            }
+
+            compteur++;
+        }
+
+    }
+}
