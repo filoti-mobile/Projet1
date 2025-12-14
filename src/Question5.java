57c168a42d9b4f03b0d64c69207280c50a193e69
toscanoGithub <pasarela70@hotmail.com>
2025-12-13 22:45:08 -0500

    Réponse 5

---
+++ src/Question5.java
@@ -0,0 +1,16 @@
+/* Demandez à l’usager d’entrer deux valeurs, a et b. Ensuite, calculez et affichez le résultat de la a b somme suivante : ∑∑ i 1 = = j 1 ( i */
+
+void main() {
+    // Variables de collecte des données
+    String saisie = IO.readln("Entrez premier nombre entier: ");
+    int saisieConvertie1 = Integer.parseInt(saisie);
+
+    saisie = IO.readln("Entrez deuxième nombre entier: ");
+    int saisieConvertie2 = Integer.parseInt(saisie);
+
+    for (int i = 1; i <= saisieConvertie1 ; i++) {
+        for (int j = 1; j <= saisieConvertie2; j++) {
+            IO.println(i + " x " + j + " = " + i * j);
+        }
+    }
+}
