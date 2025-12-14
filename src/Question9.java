c26048391d64f2bcf55c95f43aee069f8c96c89d
toscanoGithub <pasarela70@hotmail.com>
2025-12-14 13:20:15 -0500

    Réponse 9

---
+++ src/Question9.java
@@ -0,0 +1,25 @@
+/* On considère un circuit de n résistances en parallèle. La valeur de la résistante équivalente est obtenue grâce à la formule suivante : */
+
+void main() {
+    // Variables et collecte des données
+    IO.print("Entrez le nombre de résistances: ");
+    Scanner scanner = new Scanner(System.in);
+    int saisieConvertie = scanner.nextInt();
+
+    // Traitement des données
+    double resistanceEquivalente;
+    double cumulDesValeursInversees = 0; // si utilisée à l'interieur du bloc for, il faut initialiser pour calmer le compilateur
+    IO.println("Entrez les valeurs des " + saisieConvertie + " résistances: ");
+    for (int i = 0; i < saisieConvertie; i++) {
+        double r = scanner.nextDouble();
+        double valeurInversee = 1.0 / r;    // inverser les valeurs des résistances
+        cumulDesValeursInversees += valeurInversee; // faire la somme des valeurs inversées
+
+    }
+
+    resistanceEquivalente = 1.0 / cumulDesValeursInversees;
+
+    // Afficher resistanceEquivalente formatée à une précision de 2 décimaux
+    IO.println(String.format("%,.2f", resistanceEquivalente));
+
+}
